0768fb722f0001b9b669b7d991568d7b2f4625b6
Thomas Kwa <kwathomas0@gmail.com>
2023-03-23 19:08:09 -0700

    added bigger periodic table

--- game.py
+++ game.py
@@ -237,9 +237,9 @@ class SpacechemGame():
     """
     Variables are an object's state at time t, BEFORE atoms move to time t+1.
     """
-    atom_types_list = ['?', 'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na']
-
-    def __init__(self, T:int, width:int=10, height:int=8, n_atom_types:int=100, max_atoms:int=80, n_waldos:int=1, max_bfs:int=10):
+    atom_types_list = \
+    '? H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No Lr Rf Db Sg Bh Hs Mt'.split()
+    def __init__(self, T:int, width:int=10, height:int=8, n_atom_types:int=109, max_atoms:int=80, n_waldos:int=1, max_bfs:int=10):
         """
         max_bfs is the maximum distance from an atom to a grabbing waldo.
         """
